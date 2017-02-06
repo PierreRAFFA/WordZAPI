549105c312771d1423dd04f8ae88890be28d09b7
PierreRAFFA <pierre.raffa@gmail.com>
2017-02-06 21:45:17 +0000

    Add MongoDB Connector - Passport and config

---
+++ server/boot/script.js
@@ -0,0 +1,54 @@
+// {"email":"admin@wordz.com", "password":"Test123"}
+
+module.exports = function(app) {
+    var User = app.models.user;
+    var Role = app.models.Role;
+    var RoleMapping = app.models.RoleMapping;
+
+    User.create([
+        {username: 'Admin', email: 'admin@wordz.com', password: 'Test123'},
+    ], function(err, users) {
+        if (err) throw err;
+
+        console.log('Created users:', users);
+
+        // create project 1 and make john the owner
+        // users[0].projects.create({
+        //     name: 'project1',
+        //     balance: 100
+        // }, function(err, project) {
+        //     if (err) throw err;
+        //
+        //     console.log('Created project:', project);
+        //
+        //     // add team members
+        //     Team.create([
+        //         {ownerId: project.ownerId, memberId: users[0].id},
+        //         {ownerId: project.ownerId, memberId: users[1].id}
+        //     ], function(err, team) {
+        //         if (err) throw err;
+        //
+        //         console.log('Created team:', team);
+        //     });
+        // });
+
+        //create the admin role
+        Role.create({
+            name: 'admin'
+        }, function(err, role) {
+            if (err) throw err;
+
+            console.log('Created role:', role);
+
+            //make bob an admin
+            role.principals.create({
+                principalType: RoleMapping.USER,
+                principalId: users[0].id
+            }, function(err, principal) {
+                if (err) throw err;
+
+                console.log('Created principal:', principal);
+            });
+        });
+    });
+};
