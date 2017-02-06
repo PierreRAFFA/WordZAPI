867b6974f03429a997cbebc18587c6e23ca55120
PierreRAFFA <pierre.raffa@gmail.com>
2017-02-06 22:10:34 +0000

    Create Admin user only if not exist

--- server/boot/script.js
+++ server/boot/script.js
@@ -5,50 +5,54 @@ module.exports = function(app) {
     var Role = app.models.Role;
     var RoleMapping = app.models.RoleMapping;
 
-    User.create([
-        {username: 'Admin', email: 'admin@wordz.com', password: 'Test123'},
-    ], function(err, users) {
-        if (err) throw err;
+    User.find({ where: { username: 'Admin' }}, function(err, user) {
+       if (user.length === 0) {
+           User.create([
+               {username: 'Admin', email: 'admin@wordz.com', password: 'Test123'},
+           ], function(err, users) {
+               if (err) throw err;
 
-        console.log('Created users:', users);
+               console.log('Created users:', users);
 
-        // create project 1 and make john the owner
-        // users[0].projects.create({
-        //     name: 'project1',
-        //     balance: 100
-        // }, function(err, project) {
-        //     if (err) throw err;
-        //
-        //     console.log('Created project:', project);
-        //
-        //     // add team members
-        //     Team.create([
-        //         {ownerId: project.ownerId, memberId: users[0].id},
-        //         {ownerId: project.ownerId, memberId: users[1].id}
-        //     ], function(err, team) {
-        //         if (err) throw err;
-        //
-        //         console.log('Created team:', team);
-        //     });
-        // });
+               // create project 1 and make john the owner
+               // users[0].projects.create({
+               //     name: 'project1',
+               //     balance: 100
+               // }, function(err, project) {
+               //     if (err) throw err;
+               //
+               //     console.log('Created project:', project);
+               //
+               //     // add team members
+               //     Team.create([
+               //         {ownerId: project.ownerId, memberId: users[0].id},
+               //         {ownerId: project.ownerId, memberId: users[1].id}
+               //     ], function(err, team) {
+               //         if (err) throw err;
+               //
+               //         console.log('Created team:', team);
+               //     });
+               // });
 
-        //create the admin role
-        Role.create({
-            name: 'admin'
-        }, function(err, role) {
-            if (err) throw err;
+               //create the admin role
+               Role.create({
+                   name: 'admin'
+               }, function(err, role) {
+                   if (err) throw err;
 
-            console.log('Created role:', role);
+                   console.log('Created role:', role);
 
-            //make bob an admin
-            role.principals.create({
-                principalType: RoleMapping.USER,
-                principalId: users[0].id
-            }, function(err, principal) {
-                if (err) throw err;
+                   //make bob an admin
+                   role.principals.create({
+                       principalType: RoleMapping.USER,
+                       principalId: users[0].id
+                   }, function(err, principal) {
+                       if (err) throw err;
 
-                console.log('Created principal:', principal);
-            });
-        });
+                       console.log('Created principal:', principal);
+                   });
+               });
+           });
+       }
     });
 };
